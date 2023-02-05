8cae29becf3160b434736babf5969aaf2f003405
lisongmiller <72149522+lisongmiller@users.noreply.github.com>
2023-02-05 10:41:24 +0100

    Update test_dineshkumar.R

--- programs/test_dineshkumar.R
+++ programs/test_dineshkumar.R
@@ -2,6 +2,8 @@
 install.packages("usethis")
 usethis::use_git_config(user.name ="dineshkumarchenna",
                         user.email="cdineshkumarr@gmail.com")
-usethis::create_github_token()
 
 gitcreds::gitcreds_set()
+
+#after running last command, type in this token in the console to set the working environment
+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
